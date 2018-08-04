19ba9c5664657915fb93ff9a28b75a9cb83f55fc
SlowV <31509114+SlowV@users.noreply.github.com>
2018-08-04 19:19:33 +0700

    exam-php

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,198 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('apartments')->truncate();
+        DB::table('apartments')->insert([
+            [
+                'id' => 1,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 2,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 3,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 4,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 5,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 6,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 7,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 8,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 9,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 10,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 11,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 12,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 13,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 14,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 15,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 16,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 17,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 18,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 19,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+            [
+                'id' => 20,
+                'name' =>'Chung cư Xuân Phương Residence',
+                'address' => 'Xuân Phương - Hà Nội',
+                'price' => '19900000',
+                'images' => 'https://media.ohay.tv/v1/content/2015/12/keangnam-hanoi-landmark-tower-shared-photo-us-6512bd43d9caa6e02c990b0a82652dca-smaller-11-ohay-tv-63478.jpg',
+                'description' => 'Chung cư đẹp.',
+                'detail' => 'Áo sơ mi nam lịch lãm',
+            ],
+        ]);
+    }
+}
